7abbfd4da3808c847c19966a33f60c3e6c7eb1dc
/media/remote_home/cgrund <privat@christian-grund.de>
2019-05-24 13:34:34 +0200

    Improved performance. One object per label-class instead of one object per data-point.

--- blendplot.py
+++ blendplot.py
@@ -8,17 +8,57 @@ import random
 import numpy
 import scipy.io
 
-rootDir= '/home/garbade/datasets/sscnet/data/mg_gt/'
+rootDir= '/media/remote_home/cgrund/Downloads/'
 
-file = 'NYU0077_0000.mat'
+file = 'gt_256x256x32_11_000050.mat'
 
 print(rootDir + file)
-data = scipy.io.loadmat( rootDir + file)['label'] 
+data = scipy.io.loadmat( rootDir + file)['data'] 
 
 print(data.shape)
-data = np.transpose(data, (0, 2, 1))
+voxel_dims = np.array([256, 256, 32])
+data = np.reshape(data, (voxel_dims))
+data = np.transpose(data, (1, 0, 2))
 context = bpy.context
 scene = context.scene
+
+def generate_blocks(scn,points,name="MyObject"):
+    mesh = bpy.data.meshes.new("mesh")  # add a new mesh
+    obj = bpy.data.objects.new(name, mesh)
+    scene = bpy.context.scene
+    scene.objects.link(obj)  # put the object into the scene (link)
+    scene.objects.active = obj
+    obj.select = True  # select object
+    mesh = bpy.context.object.data
+    bm = bmesh.new()
+    #                     0          1       2         3         4         5       6        7
+    block=np.array([ [-1,-1,-1],[-1,-1,1],[-1,1,-1],[-1,1,1],[1,-1,-1],[1,-1,1],[1,1,-1],[1,1,1]]).astype(float)
+    block*=0.4
+    verts = np.empty(shape=(0,3))
+    for i in points:
+        #print((block+i).shape)
+        verts=np.append(verts, (block+i),axis=0)
+        
+    #print(verts)
+    for v in verts:
+        bm.verts.new(v)
+    bm.to_mesh(mesh)
+    bm.verts.ensure_lookup_table()
+    for i in range(0,len(bm.verts),8):
+        bm.faces.new( [bm.verts[i+0], bm.verts[i+1],bm.verts[i+3], bm.verts[i+2]])
+        bm.faces.new( [bm.verts[i+4], bm.verts[i+5],bm.verts[i+1], bm.verts[i+0]])
+        bm.faces.new( [bm.verts[i+6], bm.verts[i+7],bm.verts[i+5], bm.verts[i+4]])
+        bm.faces.new( [bm.verts[i+2], bm.verts[i+3],bm.verts[i+7], bm.verts[i+6]])
+        bm.faces.new( [bm.verts[i+5], bm.verts[i+7],bm.verts[i+3], bm.verts[i+1]]) #top
+        bm.faces.new( [bm.verts[i+0], bm.verts[i+2],bm.verts[i+6], bm.verts[i+4]]) #bottom
+    if bpy.context.mode == 'EDIT_MESH':
+        bmesh.update_edit_mesh(obj.data)
+    else:
+        bm.to_mesh(obj.data)
+    obj.data.update()
+    bm.free
+    return obj
+
 
 def clear_scene(scene):
     #Clears the current scene
@@ -36,29 +76,47 @@ def norm_color(color):
     return color
 
 #Set the colors for the classes
-colors = {  '_1':(    0.8400,   0.1500,   0.1600),
-            '_2':(    0.1700,   0.6300,   0.1700),
-            '_3':(    0.6200,   0.8500,   0.9000),
-            '_4':(    0.4500,   0.6200,   0.8100),
-            '_5':(    0.8000,   0.8000,   0.3600),
-            '_6':(    1.0000,   0.7300,   0.4700),
-            '_7':(    0.5800,   0.4000,   0.7400),
-            '_8':(    0.1200,   0.4700,   0.7100),
-            '_9':(    0.7400,   0.7400,   0.1300),
-            '_10':(    1.0000,   0.5000,   0.0500),
-            '_11':(    0.7700,   0.6900,   0.8400)
+colors = {  '_3':(0.392157,0.588235,0.960784),
+	 '_4':(0.392157,0.901961,0.960784),
+	 '_5':(0.392157,0.313725,0.980392),
+	 '_6':(0.117647,0.235294,0.588235),
+	 '_7':(0.000000,0.000000,1.000000),
+	 '_8':(0.313725,0.117647,0.705882),
+	 '_9':(0.000000,0.000000,1.000000),
+	 '_10':(1.000000,0.117647,0.117647),
+	 '_11':(1.000000,0.156863,0.784314),
+	 '_12':(0.588235,0.117647,0.352941),
+	 '_13':(1.000000,0.000000,1.000000),
+	 '_14':(1.000000,0.588235,1.000000),
+	 '_15':(0.294118,0.000000,0.294118),
+	 '_16':(0.686275,0.000000,0.294118),
+	 '_17':(1.000000,0.784314,0.000000),
+	 '_18':(1.000000,0.470588,0.196078),
+	 '_19':(1.000000,0.588235,0.000000),
+	 '_20':(0.588235,1.000000,0.666667),
+	 '_21':(0.000000,0.686275,0.000000),
+	 '_22':(0.529412,0.235294,0.000000),
+	 '_23':(0.588235,0.941176,0.313725),
+	 '_24':(1.000000,0.941176,0.588235),
+	 '_25':(1.000000,0.000000,0.000000),
+	 '_26':(0.196078,1.000000,1.000000),
+	 '_27':(0.392157,0.588235,0.960784),
+	 '_28':(0.000000,0.000000,1.000000),
+	 '_29':(1.000000,0.156863,0.784314),
+	 '_30':(1.000000,0.117647,0.117647),
+	 '_31':(0.588235,0.117647,0.352941),
+	 '_32':(0.392157,0.313725,0.980392),
+	 '_33':(0.313725,0.117647,0.705882),
+	 '_34':(0.000000,0.000000,1.000000)
         }
-color_map = {1: '_1',
-            2: '_2',
-            3: '_3',
-            4: '_4',
-            5: '_5',
-            6: '_6',
-            7: '_7',
-            8: '_8',
-            9: '_9',
-            10: '_10',
-            11: '_11'
+color_map = {
+    10: '_3',
+	20: '_9',
+	30: '_10',
+	40: '_13',
+	50: '_17',
+	70: '_21',
+	80: '_24',
             }
 
 def creatematerial(name, col):
@@ -87,31 +145,20 @@ materials=dict()
 for k,v, in colors.items():
     materials[k] = creatematerial(k,v)
 
-#Create a base object to copy from
-bo = base_object()
-mesh = bo.data
 for label in color_map:
     print(label)
 
     col = colors[ color_map[label] ]
     col = norm_color(col)
     xx, yy, zz = numpy.where(data == label)
-    bo.data.materials.clear()
-    bo.data.materials.append (materials[color_map[label]])
-    for i in range(len(xx)):
-        (x,y,z) = (xx[i], -yy[i]+60, zz[i])
-        thiscube = bo.copy()
-        thiscube.data = thiscube.data.copy()
-        scene.objects.link(thiscube)
-        thiscube.location=(x,y,z)
-        thiscube.parent=empty
+    points=[[xx[i], -yy[i], zz[i]] for i in range(len(xx))]
+    obj=generate_blocks(scene,points,"Label {}".format(label))
+    obj.data.materials.clear()
+    obj.data.materials.append(materials[color_map[label]])
+    obj.parent=empty
         
-#Delete the base object. It's not needed anymore
-scene.objects.unlink(bo)
-bpy.data.objects.remove(bo)
-
 bpy.ops.object.camera_add(location=(-50,95,55),rotation=(1.18683,0,4)) #Set camera location
-bpy.context.object.data.clip_end = 200 #Extend Clip length so that far away voxels are plotted aswell
+bpy.context.object.data.clip_end = np.max(np.max(data))*4 #Extend Clip length so that far away voxels are plotted aswell
 point=bpy.ops.object.lamp_add(type='POINT',location=(15,15,60)) 
 bpy.ops.object.lamp_add(type='SUN',location=(15,15,80))
 #The sun intensity needs to be adjusted afterwords manually. If someone can automate this feel free to make a push request.
